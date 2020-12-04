6fa0a4af5681908cc2347bd8867c427c0d735889
srushti-714 <55180169+srushti-714@users.noreply.github.com>
2020-12-04 16:32:18 +0530

    Create logontask.ps1

---
+++ artifacts/environment-setup/spektra/logontask.ps1
@@ -0,0 +1,12 @@
+Start-Transcript -Path C:\WindowsAzure\Logs\logontasklogs.txt -Append
+cd 'C:\LabFiles\synapse-ws-L300\artifacts\environment-setup\automation'
+
+#Environment setup script
+./01-environment-setup.ps1
+
+#Validation scripts
+./07-01-environment-poc-pre-validate.ps1
+./07-02-environment-poc-validate.ps1
+
+Unregister-ScheduledTask -TaskName "Setup" -Confirm:$false
+Stop-Transcript
